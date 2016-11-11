c482f3d5125e132e70e367fff742a18c1c0a1f5e
wdw110 <wudw110@gmail.com>
2016-11-11 11:50:24 +0800

    finish main content about qiushi script

--- qiushi_spider.py
+++ qiushi_spider.py
@@ -1,18 +1,28 @@
 #encoding=utf-8
 
+import re
 import urllib
 import urllib2
 
-page = 1
+page = 2
 url = 'http://www.qiushibaike.com/hot/page/' + str(page)
-user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-headers = { 'User-Agent' : user_agent }
+headers = {  
+   'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'  
+}  
+
 try:
-	request = urllib2.Request(url)
+	request = urllib2.Request(url,headers=headers)
 	response = urllib2.urlopen(request)
-	print response.read()
+	#print response.read()
 except urllib2.URLError as e:
 	if hasattr(e,'code'):
 		print e.code
 	if hasattr(e,'reason'):
 		print e.reason
+
+content = response.read().decode('utf-8')
+pattern = re.compile('<div class="author clearfix">.*?href.*?<img src.*?title=.*?<h2>(.*?)</h2>.*?<div class="content">(.*?)</div>.*?<i class="number">(.*?)</i>',re.S)
+items = re.findall(pattern,content)
+for item in items:
+	print item[0],item[1],item[2]
+
